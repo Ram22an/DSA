185ed0d03756132b01f838b1ae45eabd38250d06
Ram22an <118821139+Ram22an@users.noreply.github.com>
2024-08-14 01:12:28 +0530

    Frequency array  update

--- Hashing/CountArrayElement.py
+++ Hashing/CountArrayElement.py
@@ -1,11 +1,15 @@
 # here is one constraine like if we enter 5 then array is of 5 size 
 # and also element smaller than or equal to 5
 Size=int(input())
-Frequency=[0]*(Size+1)
+Frequency=[0]*(10**8+1)
 Arr=[]
 for i in range(Size):
     temp=int(input())
     Arr.append(temp)
     Frequency[temp]+=1
 print(Arr)
-print([f"{i} {index}"for i , index in enumerate (Frequency)])
+Query=int(input("Enter the number of query "))
+for _ in range(Query):
+    temp=int(input())
+    print(Frequency[temp])
+# print([f"{i} {index}"for i , index in enumerate (Frequency)])
