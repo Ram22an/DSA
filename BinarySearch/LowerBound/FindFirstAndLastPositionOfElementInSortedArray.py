b9c8b4901418147e7cf5609ef9edb2f396e59001
Ram22an <118821139+Ram22an@users.noreply.github.com>
2025-02-18 21:48:00 +0530

    Updated Find First and Last Position using Lower Bound and Upper Bound Binary Search

--- BinarySearch/LowerBound/FindFirstAndLastPositionOfElementInSortedArray.py
+++ BinarySearch/LowerBound/FindFirstAndLastPositionOfElementInSortedArray.py
@@ -32,4 +32,35 @@
 #     print(ansLowerBound,ansUpperBound)
 # it is very tough question it is not done by me i will try it tomorrow or later
 
-        
+
+# this i tried to understand the question and implemented the logic
+nums=list(map(int,input().strip().split()))
+target=int(input())
+StartingPosition=-1
+EndingPosition=-1
+# here we will find upper bound that is arr[ind]>x
+left=0
+right=len(nums)-1
+mid=0
+while left<=right:
+    mid=(left+right)//2
+    if nums[mid]>target:
+        right=mid-1
+    else:
+        left=mid+1
+EndingPosition=mid-1
+
+# here we will find lower bound that is arr[ind]>=x
+left=0
+right=len(nums)-1
+mid=0
+while left<=right:
+    mid=(left+right)//2
+    if nums[mid]>=target:
+        right=mid-1
+    else:
+        left=mid+1
+if StartingPosition==len(nums) or nums[StartingPosition]!=target:
+    print(-1,-1)
+else:
+    print(StartingPosition,EndingPosition)
